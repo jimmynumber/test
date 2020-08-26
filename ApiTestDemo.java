1b92a80b1dbc11e3b900949213bcdb7ff0c3bacf
jimmynumber <68052692+jimmynumber@users.noreply.github.com>
2020-08-26 11:39:12 +0800

    Update ApiTestDemo.java
    1

--- ApiTestDemo.java
+++ ApiTestDemo.java
@@ -1,3 +1,11 @@
+/*
+$ git init
+$ git add .
+$ git commit -m "first commit"
+$ git remote add origin https://github.com/jimmynumber/test.git
+$ git push -u origin master
+*/
+
 package com.want;
 
 import java.io.BufferedReader;
